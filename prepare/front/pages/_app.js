b99cd4adc3f90bd17a1c414c1ef25ecb91309c57
sg1313 <fpemvps2@gmail.com>
2023-01-27 13:10:53 +0900

    next-redux-wrapper@6

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -5,8 +5,12 @@ import Head from "next/head";
 import "antd/dist/antd.css";
 import { PropTypes } from "prop-types";
 
+import wrapper from "../store/configureStore";
+
 const NodeBird = ({ Component }) => {
   return (
+    // Redux 쓰면 아래 <div>자리에 <Provider stroe=[store>/] 라는걸 넣는다. page를 provider로 감싸줌. 근데 next에서는 provider 안써도 된다.
+    // next-redux-wrapper에서 provider를 제공해주기 때문에 오히려 넣어주면 중복이 발생함
     <div>
       <Head>
         <meta charSet="utf-8" />
@@ -21,4 +25,4 @@ NodeBird.prototype = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);
